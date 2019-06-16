8c832ce6b63b54404a06d4c2ad657798c481817f
Wayne Wolf <bluecupdesign@Waynes-Air.home>
2019-06-16 13:21:41 -0400

    Fixed Name Display Bug

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const change = () => {
   const a = document.getElementById("name-input-a").value;
   const b = document.getElementById("name-input-b").value;
   const c = document.getElementById("name-input-c").value;
-  const d = document.getElementById("name-input-c").value;
-  const e = document.getElementById("name-input-c").value;
-  const f = document.getElementById("name-input-c").value;
+  const d = document.getElementById("name-input-d").value;
+  const e = document.getElementById("name-input-e").value;
+  const f = document.getElementById("name-input-f").value;
   document.getElementById("name-output-a").innerHTML = a ;
   document.getElementById("name-output-b").innerHTML = b ;
   document.getElementById("name-output-c").innerHTML = c ;
