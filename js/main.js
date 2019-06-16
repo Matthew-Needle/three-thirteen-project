f991171fc59b7cfefac842ad29d8183fcdf010a1
Wayne Wolf <bluecupdesign@Waynes-Air.home>
2019-06-16 02:55:35 -0400

    Add Player Creation Functionality / Grid Column Creation

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*Player Change Function*/
+const change = () => {
+  const a = document.getElementById("name-input-a").value;
+  const b = document.getElementById("name-input-b").value;
+  const c = document.getElementById("name-input-c").value;
+  const d = document.getElementById("name-input-c").value;
+  const e = document.getElementById("name-input-c").value;
+  const f = document.getElementById("name-input-c").value;
+  document.getElementById("name-output-a").innerHTML = a ;
+  document.getElementById("name-output-b").innerHTML = b ;
+  document.getElementById("name-output-c").innerHTML = c ;
+  document.getElementById("name-output-d").innerHTML = d ;
+  document.getElementById("name-output-e").innerHTML = e ;
+  document.getElementById("name-output-f").innerHTML = f ;
+};
+
+/*Player Change Enter Button*/
+
+$(document).ready(()=> {
+
+  $('#enter-players').on('click', () => {
+    change();
+  });
+});
